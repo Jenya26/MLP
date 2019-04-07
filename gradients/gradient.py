fix error with shapes for gradient

gradients/gradient.py
@@ -24,8 +24,8 @@ class Gradient:
         activation_function_gradient = output_gradient * layer.activation_function.derivative(inputs, outputs)
         weights_gradient = np.zeros((layer.input_dimension, layer.output_dimension))
         for i in range(inputs.shape[0]):
-            current_input = inputs[i].T
-            current_output = activation_function_gradient[i]
+            current_input = inputs[i].reshape((layer.input_dimension, 1))
+            current_output = activation_function_gradient[i].reshape((1, layer.output_dimension))
             weights_gradient += np.dot(current_input, current_output)
         biases_gradient = np.sum(activation_function_gradient, axis=0)
         gradient += [[

gradients/tests/gradient_test.py
@@ -7,43 +7,45 @@ from mlp import NeuralNetwork, Layer
 from errors import SquareError
 from gradients import Gradient
 
-__all__ = ['should_success_calculate_for_multiple_examples']
-
-network = NeuralNetwork([
-    Layer(
-        input_dimension=1,
-        output_dimension=1,
-        activation_function=LinearFunction(),
-        weights_initializer=ConstShapeInitializer(
-            np.asarray([
-                [1.]
-            ])
-        ),
-        biases_initializer=ConstShapeInitializer(
-            np.asarray(
-                [2.]
-            )
-        )
-    ),
-    Layer(
-        input_dimension=1,
-        output_dimension=1,
-        activation_function=LinearFunction(2.),
-        weights_initializer=ConstShapeInitializer(
-            np.asarray([
-                [3.]
-            ])
-        ),
-        biases_initializer=ConstShapeInitializer(
-            np.asarray(
-                [0.]
-            )
-        )
-    )
-])
+__all__ = [
+    'should_success_calculate_for_multiple_examples',
+    'should_success_calculate_for_multiple_neurons'
+]
 
 
 def should_success_calculate_for_multiple_examples():
+    network = NeuralNetwork([
+        Layer(
+            input_dimension=1,
+            output_dimension=1,
+            activation_function=LinearFunction(),
+            weights_initializer=ConstShapeInitializer(
+                np.asarray([
+                    [1.]
+                ])
+            ),
+            biases_initializer=ConstShapeInitializer(
+                np.asarray(
+                    [2.]
+                )
+            )
+        ),
+        Layer(
+            input_dimension=1,
+            output_dimension=1,
+            activation_function=LinearFunction(2.),
+            weights_initializer=ConstShapeInitializer(
+                np.asarray([
+                    [3.]
+                ])
+            ),
+            biases_initializer=ConstShapeInitializer(
+                np.asarray(
+                    [0.]
+                )
+            )
+        )
+    ])
     X = np.asarray([[0.],
                    [1.]])
     Y = np.asarray([[0.],
@@ -66,6 +68,71 @@ def should_success_calculate_for_multiple_examples():
             ]),
             np.asarray(
                 [112.]
+            )
+        ]
+    ])
+    equals(expected, network_gradient)
+
+
+def should_success_calculate_for_multiple_neurons():
+    network = NeuralNetwork([
+        Layer(
+            input_dimension=1,
+            output_dimension=3,
+            activation_function=LinearFunction(),
+            weights_initializer=ConstShapeInitializer(
+                np.asarray([
+                    [1., 2., 3.]
+                ])
+            ),
+            biases_initializer=ConstShapeInitializer(
+                np.asarray(
+                    [1., 2., 3.]
+                )
+            )
+        ),
+        Layer(
+            input_dimension=3,
+            output_dimension=1,
+            activation_function=LinearFunction(2.),
+            weights_initializer=ConstShapeInitializer(
+                np.asarray([
+                    [1.],
+                    [2.],
+                    [3.]
+                ])
+            ),
+            biases_initializer=ConstShapeInitializer(
+                np.asarray(
+                    [1.]
+                )
+            )
+        )
+    ])
+    X = np.asarray([[0.],
+                   [1.]])
+    Y = np.asarray([[0.],
+                   [2.]])
+    gradient = Gradient()
+    square_error = SquareError()
+    network_gradient = gradient(network, X, Y, square_error(Y, network(X), 1))
+    expected = np.asarray([
+        [
+            np.asarray([
+                [224., 448., 672.]
+            ]),
+            np.asarray(
+                [344.,  688., 1032.]
+            )
+        ],
+        [
+            np.asarray([
+                [568.],
+                [1136.],
+                [1704.]
+            ]),
+            np.asarray(
+                [344.]
             )
         ]
     ])