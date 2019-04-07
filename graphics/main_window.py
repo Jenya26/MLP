87fc8c690f569881671e8cf171cfadbc4d6d75ae
Eugene Magdin <Jenya266@gmail.com>
2019-04-07 22:22:19 +0600

    refactoring main window

--- graphics/main_window.py
+++ graphics/main_window.py
@@ -20,22 +20,27 @@ CHART_TITLE = "Neural network"
 class NeuralNetworkWindow(QMainWindow):
     def __init__(self, network_model, parent=None):
         super(NeuralNetworkWindow, self).__init__(parent=parent)
+        self._network_model = network_model
         self._chart_widget = ChartWidget(CHART_TITLE)
-        network = network_model.models[0].base_model
-        originalValues = network_model.models[0].original.values
+        self.setCentralWidget(self._chart_widget)
+        self.update_chart()
+
+    def update_chart(self):
+        network = self._network_model.models[0].base_model
+        originalValues = self._network_model.models[0].original.values
         xOriginalValues = originalValues[:, 0].reshape((originalValues.shape[0], 1))
         yNetworkValues = network(xOriginalValues)
+        trainValues = self._network_model.models[0].train.values
         networkValues = np.concatenate((xOriginalValues, yNetworkValues), axis=1)
         self._original_line_series = self._chart_widget.create_line_series(
             originalValues, color=Qt.green
         )
         self._train_scatter_series = self._chart_widget.create_scatter_series(
-            network_model.models[0].train.values, color=QColor(255, 0, 0)
+            trainValues, color=QColor(255, 0, 0)
         )
         self._network_line_series = self._chart_widget.create_line_series(
             networkValues, color=QColor(255, 165, 0)
         )
-        self.setCentralWidget(self._chart_widget)
 
 
 if __name__ == '__main__':
